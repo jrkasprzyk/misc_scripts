049f80338f83776dda5d53a251c00498de4cb0e5
Joseph Kasprzyk <joseph.kasprzyk@colorado.edu>
2013-04-25 08:54:13 -0400

    fixed indices, problem specific calls
    Various changes.  Fixed a problem with indexing, also made it so that
    each individual problem has its own threshold files.  Added some
    optional debugging output.

--- callMOEAFrameworkAnalysis.py
+++ callMOEAFrameworkAnalysis.py
@@ -48,17 +48,21 @@ def main():
     javaCall = ['/usr/bin/java']
     javaProgram = ['org.moeaframework.analysis.sensitivity.Analysis']
     invariantParams = ['--parameterFile', './BORG_Params.txt', '--parameters', './paramValues/BORG_Param_Values.txt', '--efficiency']
-    hypervolumeValue = 0.01
-    hypervolumeArg = ['--hypervolume', str(hypervolumeValue)]
-    
+      
     #...Including the specific problems we want to calculate
-    problems = ['AllDecAll', 'AllDecCostRel', 'LowDecAll', 'LowDecCostRel']
-    problemFilenames = ['./metrics/' + problem + '.average' for problem in problems]
-    #Insert reference set hypervolume here!    
-    
+    problems = ['AllDecAll', 'LowDecAll', 'AllDecCostRel', 'LowDecCostRel']
+    problemFilenames = ['./metrics/' + problem + '.average' for problem in problems] 
+    problemIndices = range(0,4)
+
+    #The following problem hypervolumes are from the "Without Recency" experiment:
+    #used to use 0.135184794908381, the hypervolume of the file solutionsFourProblems.noViolation.sorted.txt
+    problemHypervolumeValues = ['0.29711526952760375', '0.11603091662956089', '0.485822499851039', '0.5838159649759032'] 
 
     #Now get ready for the looping.
-    #Add one iteration to the end point for these commands to get the full list
+    #Note: The thresholds array below was created with the numpy library,
+    #and the function 'arange'.  On the cluster, numpy is not installed
+    #so I just hard-coded the thresholds here.  Anyway, it's probably better
+    #because there's no ambiguity about the step size or the end points.
     thresholds = ([0.01,  0.02,  0.03,  0.04,  0.05,  0.06,  0.07,  0.08,
         0.09,  0.1 ,  0.11,  0.12,  0.13,  0.14,  0.15,  0.16,  0.17,
         0.18,  0.19,  0.2 ,  0.21,  0.22,  0.23,  0.24,  0.25,  0.26,
@@ -71,34 +75,53 @@ def main():
         0.81,  0.82,  0.83,  0.84,  0.85,  0.86,  0.87,  0.88,  0.89,
         0.9 ,  0.91,  0.92,  0.93,  0.94,  0.95,  0.96,  0.97,  0.98,
         0.99,  1.  ])
-    percentiles = range(1,101)
-    metricIndices = range(0,6)
+    percentiles = range(1,101) 
+    metricIndices = range(0,5)
     metricNames = ['Hyp', 'GD', 'InGD', 'Spacing', 'EpsInd', 'MaxPFE']
+
+    print "metric indices"
+    print metricIndices
+    print "problem indices"
+    print problemIndices
     
     counter = 0  
     for threshold in thresholds:            
         for metricIndex in metricIndices:
-            metricName = metricNames[metricIndex-1]
+            for problemIndex in problemIndices:
+
+                #For this particular iteration, assign problem and metricName
+                problem = problems[problemIndex]
+                metricName = metricNames[metricIndex]
+
+                #Turn on for debugging
+                #print 'threshold, metricIndex, problemIndex: %f %d %d' % (threshold, metricIndex, problemIndex)
+                #print 'therefore problem is %s, metricName is %s' % (problem, metricName)                
+
+                #Construct the arguments specific to this call
+                metricArg = ['--metric', str(metricIndex)]
+                thresholdArg = ['--threshold', str(threshold)]
+                outputArg = ['--output', './analysis/' + problem + '_' + metricName + '_' + str(percentiles[counter]) + '.txt']
+                hypervolumeArg = ['--hypervolume', problemHypervolumeValues[problemIndex]]
+    
+                if metricName == 'Hyp':
+                    #command specific to hypervolume must include the reference set hypervolume
+                    cmd = ( javaCall + javaArgs + javaProgram + invariantParams + metricArg
+                        + thresholdArg + hypervolumeArg + outputArg + [problemFilenames[problemIndex]])
+                else:
+                    cmd = ( javaCall + javaArgs + javaProgram + invariantParams + metricArg
+                        + thresholdArg + outputArg + [problemFilenames[problemIndex]])
                 
-            #Construct the arguments specific to this call
-            metricArg = ['--metric', str(metricIndex)]
-            thresholdArg = ['--threshold', str(threshold)]
-            outputArg = ['--output', './analysis/' + metricName + '_' + str(percentiles[counter]) + '.txt']
+                #Now that we've constructed the command, we check it for correctness...
+                print cmd
                 
-            if metricName == 'Hyp':
-                #command specific to hypervolume must include the reference set hypervolume
-                cmd = ( javaCall + javaArgs + javaProgram + invariantParams + metricArg
-                    + thresholdArg + hypervolumeArg + outputArg + problemFilenames)
-            else:
-                cmd = ( javaCall + javaArgs + javaProgram + invariantParams + metricArg
-                    + thresholdArg + outputArg + problemFilenames)
-                
-            #Now that we've constructed the command, we check it for correctness, then fork a process to
-            #run it.
-            print cmd
-                
-            child = Popen(cmd, cwd='//work//00868//tg459235//LRGV_2013-03-10_runs-without-recency//parallel-test-256samples', bufsize=LINE_BUFFERED, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-            child.wait()            
+                #...then fork a process to run it.  Turn this off for debugging purposes.
+                child = Popen(cmd, cwd='//work//00868//tg459235//LRGV_2013-03-10_runs-without-recency//parallel-test-256samples', bufsize=LINE_BUFFERED, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                child.wait()
+
+                #WARNING: If the child process above has any sort of error come out of it, you won't know about it unless you
+                #process the stdin and stdout from the PIPE.  This isn't hard to do, so you can add it here if you'd like (above
+                #the wait())
+            
         counter = counter + 1
 if __name__ == "__main__":
     main()
