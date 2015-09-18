f581e05a36f0b5fdb70bdbae6ed16776c1f8fbc8
Joseph Kasprzyk <joseph.kasprzyk@colorado.edu>
2015-09-18 17:14:45 -0600

    Added license info

--- createBorgRunCommand.py
+++ createBorgRunCommand.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2015 Joseph Kasprzyk
+
+# This script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with the script.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Program description:
+# A set of several classes that enable the user to construct a simple Borg running command and submit it
+# using a system subprocess command.  Requires OrderedDict from collections, and subprocess.
+
 from collections import OrderedDict
 import subprocess
 
