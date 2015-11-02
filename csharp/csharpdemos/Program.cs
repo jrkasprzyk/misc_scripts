e6e53b886f4282ecbdd7030cfa3baf9691ee573e
Joseph Kasprzyk <joseph.kasprzyk@colorado.edu>
2015-11-02 10:50:07 -0500

    Added inheritance and asynchronous functions

--- csharp/csharpdemos/Program.cs
+++ csharp/csharpdemos/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
+/// <summary>
+/// Playing around with the tutorial here: http://csharp.net-tutorials.com/classes/inheritance/
+/// </summary>
 
 namespace csharpdemos
 {
@@ -22,9 +27,9 @@ namespace csharpdemos
     class Car
     {
         //variable declarations
-        private string color;
-        private string make;
-        private string model;
+        protected string color;
+        protected string make;
+        protected string model;
 
         //constructor
         public Car(string color, string make, string model)
@@ -51,7 +56,7 @@ namespace csharpdemos
         }
 
         //functions
-        public string Describe()
+        public virtual string Describe()
         {
             return "This is a " + color + " " + make + " " + model + "!";
         }
@@ -60,6 +65,65 @@ namespace csharpdemos
         {
             this.color = newColor;
             return;
+        }
+    }
+
+    class SportsCar : Car
+    {
+        private string seatType;
+
+        public SportsCar(string color, string make, string model, string seatType) : base(color, make, model)
+        {
+            this.seatType = seatType;
+        }
+
+        public string SeatType
+        {
+            get
+            {
+                return seatType;
+            }
+            set { this.seatType = value;  }
+        }
+
+        public override string Describe()
+        {
+            return "This is a " + color + " " + make + " " + model + ". Since its a sports car it also has " + seatType + " seats!";
+        }
+
+    }
+
+    public static class AsyncTestbed
+    {
+        //https://msdn.microsoft.com/en-us/library/hh191443.aspx
+        public static async Task<int> AccessTheWebAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            //do the below task asynchronously
+            Console.WriteLine("Launching asynchronous task!");
+            Task<string> getStringTask = client.GetStringAsync("http://www.wsj.com/");
+
+            //do some work
+            long N = 100000;
+            long junk = 0;
+            for (long i = 0; i < N; i++)
+            {
+                junk += i*N;
+                junk = junk / 25;
+            }
+            Console.WriteLine("We are done doing that stupid work and the value of the variable is " + junk);
+
+            // The await operator suspends AccessTheWebAsync.
+            //  - AccessTheWebAsync can't continue until getStringTask is complete.
+            //  - Meanwhile, control returns to the caller of AccessTheWebAsync.
+            //  - Control resumes here when getStringTask is complete. 
+            //  - The await operator then retrieves the string result from getStringTask.
+            string urlContents = await getStringTask;
+            //Console.WriteLine("String task received, its contents are" + urlContents);
+
+            return urlContents.Length;
+
         }
     }
 
@@ -153,12 +217,17 @@ namespace csharpdemos
             Console.WriteLine("Now, showing the new function I wrote to change color...");
             emilsCar.Paint("purple");
 
-            Console.WriteLine("The make of Emils car is" + emilsCar.Make + ".");
+            Console.WriteLine("After calling the Paint method we have...");
+            Console.WriteLine(emilsCar.Describe());
 
             Console.WriteLine("Changing the make...");
             emilsCar.Make = "Ford";
 
             Console.WriteLine(emilsCar.Describe());
+
+            SportsCar joesCar;
+            joesCar = new SportsCar("red", "Nissan", "Sentra", "leather");
+            Console.WriteLine(joesCar.Describe());
 
             return;
         }
@@ -173,12 +242,16 @@ namespace csharpdemos
 
             //EnterMultipleNames();
 
-            double a = 30.0;
-            double b = 20.0;
-            double c = MyFunctionSet.AddTwoNumbers(a, b);
-            Console.WriteLine(a + " plus " + b + " is " + c);
+            //double a = 30.0;
+            //double b = 20.0;
+            //double c = MyFunctionSet.AddTwoNumbers(a, b);
+            //Console.WriteLine(a + " plus " + b + " is " + c);
 
-            CarTester();
+            //CarTester();
+
+            Task<int> n = AsyncTestbed.AccessTheWebAsync();
+
+            Console.WriteLine("n = " + n);
 
             Console.WriteLine("Hit enter to end...");
             Console.ReadLine();
